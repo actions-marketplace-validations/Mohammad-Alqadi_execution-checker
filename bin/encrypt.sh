6641d7fd23c2ce4d2304c62857075ed7e787cf47
Mohammad Alqadi <mohammad.alqadi@cds-global.com>
2022-09-01 11:39:48 +0300

    test the script 5

--- bin/encrypt.sh
+++ bin/encrypt.sh
@@ -8,6 +8,6 @@ to_pass="nothing to commit, working tree clean"
 case $status in ($to_pass)
 
     echo "$hash" | openssl pkeyutl -inkey key.txt -encrypt >output.bin
-    echo "$(<output.bin)"
-;;(*) echo "Please make sure you git add all the changes and committed!";
+    echo "$(<output.bin)";
+    echo "Please make sure you git add all the changes and committed!";
 esac
