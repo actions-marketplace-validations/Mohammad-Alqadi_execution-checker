c0d811adc6b6de0fa59f5f8659703e03ba3ece44
Mohammad Alqadi <mohammad.alqadi@cds-global.com>
2022-09-14 12:35:03 +0300

    test 222

--- bin/decrypt.sh
+++ bin/decrypt.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo -e "$1" >> ENCRYPT.txt
-echo $(openssl aes-256-cbc -d -a -pass pass:CDS-Global -pbkdf2 -iter 100000  < ENCRYPT.txt)
+echo -e "$1" >> notes.txt
+decrypt=$(openssl aes-256-cbc -d -a -pass pass:CDS-Global -pbkdf2 -iter 100000  < notes.txt)
+
+echo $decrypt
