0660c83268ef23afa1d4b1114e01e53d6d78116f
Mohammad Alqadi <mohammad.alqadi@cds-global.com>
2022-09-14 11:58:36 +0300

    release v9

--- bin/decrypt.sh
+++ bin/decrypt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-openssl aes-256-cbc -d -a -pass pass:CDS-Global -pbkdf2 -iter 100000  <"$1"
+echo -e "$2" >> ENCRYPT.txt
+openssl aes-256-cbc -d -a -pass pass:CDS-Global -pbkdf2 -iter 100000  < ENCRYPT.txt
